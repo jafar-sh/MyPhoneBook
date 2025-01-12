1e55c80fd5084f8146d2141ec5ed63b05c65cc4c
Jafar Sharifi <jafar68@gmail.com>
2025-01-12 15:51:16 -0800

    my PhoneBook make Fuctionallty

--- src/com/shrifapp/myphonebook/PhoneBook.java
+++ src/com/shrifapp/myphonebook/PhoneBook.java
@@ -4,43 +4,25 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PhoneBook {
+    private static ArrayList<String> names = new ArrayList<>();
+    private static ArrayList<String> nunbers = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> names=new ArrayList<>();
-        ArrayList<String> nunbers=new ArrayList<>();
+
         int choice;
         do {
-            System.out.println("Menu");
-            System.out.println("1.Add Contact");
-            System.out.println("2.Print All Contact");
-            System.out.println("3.Exit");
-            System.out.print("Please Enter Your Choice:");
+            showMenu();
             choice = scanner.nextInt();
             scanner.nextLine();
 
 
             switch (choice) {
                 case 1:
-                    System.out.println("Enter Name:");
-                   String name= scanner.nextLine();
-                   System.out.println("Enter Number:");
-                   String number= scanner.nextLine();
-                   names.add(name);
-                   nunbers.add(number);
-                    System.out.println("Contact Added Successfuly");
+                    addContact();
                     break;
                 case 2:
-                    if (names.isEmpty()){
-                        System.out.println("PhoneBook is Empty!");
-                    }else {
-                        System.out.println("Print All Contact");
-                    }
-                    System.out.println(" All Contacts:");
-                    for (int i = 0; i < names.size(); i++) {
-                        System.out.println("Name["+(i+1)+"]"+": "+names.get(i));
-                        System.out.println("Numberse["+(i+1)+"]"+": "+nunbers.get(i));
-
-                    }
+                    printAllContacts();
                     break;
                 case 3:
                     System.out.println("Exit!");
@@ -49,7 +31,41 @@ public class PhoneBook {
                     System.out.println("Invalid Number");
             }
 
-        }while (choice != 3);
+        } while (choice != 3);
+        scanner.close();
+    }
+
+    private static void showMenu() {
+        System.out.println("Menu");
+        System.out.println("1.Add Contact");
+        System.out.println("2.Print All Contact");
+        System.out.println("3.Exit");
+        System.out.print("Please Enter Your Choice:");
+    }
+
+    private static void printAllContacts() {
+        if (names.isEmpty()) {
+            System.out.println("PhoneBook is Empty!");
+        } else {
+            System.out.println("Print All Contact");
+        }
+        System.out.println(" All Contacts:");
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println("Name[" + (i + 1) + "]" + ": " + names.get(i));
+            System.out.println("Numberse[" + (i + 1) + "]" + ": " + nunbers.get(i));
+
+        }
+    }
+
+    private static void addContact() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Name:");
+        String name = scanner.nextLine();
+        System.out.println("Enter Number:");
+        String number = scanner.nextLine();
+        names.add(name);
+        nunbers.add(number);
+        System.out.println("Contact Added Successfuly");
         scanner.close();
     }
 }
