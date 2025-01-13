788254687ff231b5e8a3d3ae4f4a9550de6496c1
Jafar Sharifi <jafar68@gmail.com>
2025-01-12 18:08:22 -0800

    Update Contact.java Polymorphism steep3

--- src/com/shrifapp/myphonebook/Contact.java
+++ src/com/shrifapp/myphonebook/Contact.java
@@ -4,6 +4,12 @@ public class Contact {
     private String number;
     private String name;
 
+    @Override
+    public String toString() {
+        return "Name: "+name + ": Number: "+number;
+
+    }
+
     private String capitalizeFirestCharacter(String str){
         if (str!=null&& !str.isEmpty()){
             return  Character.toUpperCase(str.charAt(0))+str.substring(1);
@@ -16,6 +22,9 @@ public class Contact {
 
     public void setNumber(String number) {
         this.number = number;
+    }
+    public void setNumber(int number) {
+        this.number ="0"+ number;
     }
 
     public String getName() {
--- src/com/shrifapp/myphonebook/PhoneBook.java
+++ src/com/shrifapp/myphonebook/PhoneBook.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PhoneBook {
-//    private static ArrayList<String> names = new ArrayList<>();
-//    private static ArrayList<String> nunbers = new ArrayList<>();
+
 private static ArrayList<Contact> contacts=new ArrayList<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -52,15 +51,12 @@ private static ArrayList<Contact> contacts=new ArrayList<>();
         System.out.println(" All Contacts:");
         int i=1;
         for (Contact contact:contacts){
-            System.out.println("Name[" + i  + "]" + ": " + contact.getName());
+            System.out.println(contact.toString());
+            /*System.out.println("Name[" + i  + "]" + ": " + contact.getName());
             System.out.println("Numberse[" + i + "]" + ": " + contact.getNumber());
-            i++;
+            i++;*/
         }
-//        for (int i = 0; i < contacts.size(); i++) {
-//            System.out.println("Name[" + (i + 1) + "]" + ": " + names.get(i));
-//            System.out.println("Numberse[" + (i + 1) + "]" + ": " + nunbers.get(i));
-//
-//        }
+
     }
 
     private static void addContact(Scanner scanner) {
@@ -68,13 +64,13 @@ private static ArrayList<Contact> contacts=new ArrayList<>();
         Contact newContact=new Contact();
         System.out.println("Enter Name:");
 
-//        String name = scanner.nextLine();
+
         newContact.setName(scanner.nextLine());
-        System.out.println("Enter Number:");
-//        String number = scanner.nextLine();
-        newContact.setNumber(scanner.nextLine());
-//        names.add(name);
-//        nunbers.add(number);
+        System.out.println("Enter Number as Number only without zero at start:");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        newContact.setNumber(number);
+
         contacts.add(newContact);
         System.out.println("Contact Added Successfuly");
 
